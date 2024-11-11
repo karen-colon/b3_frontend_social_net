19d8cab088c5ed16e4ab98e5cbfd9d6441c7440d
Karen Colon <kscaraballo@hotmail.com>
2024-11-11 18:58:32 -0500

    cambios

--- src/components/Response/ReplyForm.js
+++ src/components/Response/ReplyForm.js
@@ -7,6 +7,15 @@ const ReplyForm = ({ postId, onReplySubmitted }) => {
     const handleReplyChange = (e) => {
         setReplyText(e.target.value);
     };
+    return ( < form onSubmit = { submitReply } >
+        <
+        textarea value = { replyText }
+        onChange = { handleReplyChange }
+        placeholder = "Escribe tu respuesta aquí"
+        rows = "3" / >
+        <
+        button type = "submit" > Responder < /button> </form >
+    );
 
     const submitReply = async(e) => {
         e.preventDefault();
@@ -22,20 +31,10 @@ const ReplyForm = ({ postId, onReplySubmitted }) => {
         } catch (error) {
             console.error('Error al enviar la respuesta:', error);
         }
+
     };
 
-    return ( <
-        form onSubmit = { submitReply } >
-        <
-        textarea value = { replyText }
-        onChange = { handleReplyChange }
-        placeholder = "Escribe tu respuesta aquí"
-        rows = "3" /
-        >
-        <
-        button type = "submit" > Responder < /button> <
-        /form>
-    );
+
 };
 
 export default ReplyForm;

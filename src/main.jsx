62263ab4479aa62d99f3cfd31e5c11d106a70ca4
Karen Colon <kscaraballo@hotmail.com>
2024-11-11 15:09:37 -0500

    cambios

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import TimeAgo from 'javascript-time-ago';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';  // Importar la raíz de la aplicación React
 
 // Import de assets
 import './assets/fonts/fontawesome-free-6.1.2-web/css/all.css';
@@ -10,14 +9,16 @@ import './assets/css/styles.css';
 import './assets/css/responsive.css';
 
 // Importar el paquete de localización
+import TimeAgo from 'javascript-time-ago';
 import es from 'javascript-time-ago/locale/es.json';
 
 // Registrar el idioma
 TimeAgo.addDefaultLocale(es);
 
-
+// Crear la raíz de React
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
+

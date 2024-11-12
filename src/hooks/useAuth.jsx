5c2fb14fa4d9572a8842dccf65e546ba6abb3f7e
Karen Colon <kscaraballo@hotmail.com>
2024-11-11 21:17:53 -0500

    Cambios

--- src/hooks/useAuth.jsx
+++ src/hooks/useAuth.jsx
@@ -1,11 +1,21 @@
-import { useContext } from "react"
-import { AuthContext } from "../context/AuthContext"
+import { useContext } from "react";
+import { AuthContext } from "../context/AuthContext";
 
-
+/**
+ * Custom hook para acceder al contexto de autenticación.
+ * Permite usar el contexto de AuthContext en los componentes para acceder a los estados y funciones de autenticación.
+ *
+ * @returns {object} - El valor del contexto de autenticación.
+ */
 const useAuth = () => {
+  const context = useContext(AuthContext);
 
-  // Devuelve un useContext donde cargamos el contexto del AuthContext para tener acceso a los estados y usarlos en los componentes que necesitemos
-  return useContext(AuthContext);
-}
+  // Verifica si el hook se está usando fuera de un AuthProvider
+  if (!context) {
+    throw new Error("useAuth debe usarse dentro de un AuthProvider");
+  }
+
+  return context;
+};
 
 export default useAuth;
